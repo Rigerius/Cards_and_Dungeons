3f61f78e3c2de44daf7ae2f8b4ce36b6897d5b46
Rigerius <mike.bklv@mail.ru>
2026-01-22 07:45:30 +0300

    Add files via upload

---
+++ database.py
@@ -0,0 +1,25 @@
+import sqlite3
+from dungeon_2 import dun_2
+
+for i in range(len(dun_2)):
+    dun_2[i] = '/'.join(dun_2[i])
+dun = '|'.join(dun_2)
+
+con = sqlite3.connect('database/database.db')
+cur = con.cursor()
+"""cur.execute('CREATE TABLE IF NOT EXISTS Dungeons('
+            'id INTEGER PRIMARY KEY AUTOINCREMENT,'
+            'name TEXT NOT NULL,'
+            'list TEXT NOT NULL,'
+            'start TEXT NOT NULL)')
+cur.execute('INSERT INTO Dungeons(name, list, start) VALUES (?, ?, ?)', ('dundeon_2', dun, '67, 1'))"""
+cur.execute('CREATE TABLE IF NOT EXISTS Cards('
+            'id INTEGER PRIMARY KEY AUTOINCREMENT,'
+            'name TEXT NOT NULL,'
+            'desc TEXT NOT NULL,'
+            'image TEXT NOT NULL,'
+            'damage TEXT NOT NULL,'
+            'colvo TEXT NOT NULL,'
+            'effects TEXT NOT NULL,'
+            'chance TEXT NOT NULL)')
+con.commit()
